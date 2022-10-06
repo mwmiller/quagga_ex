285aa11abbaf92847339c0a909f54bb7cb61c0c8
Matthew Miller <mwm@fastmail.com>
2022-10-06 13:58:35 +0200

    Onboard latest baby

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Quagga.MixProject do
   def project do
     [
       app: :quagga,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       releases: releases(),
