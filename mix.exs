6595fb4551ee9a10985133e748d370c9fc927ba8
Matthew Miller <mwm@fastmail.com>
2023-02-06 22:15:20 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Quagga.MixProject do
   def project do
     [
       app: :quagga,
-      version: "0.7.0",
+      version: "0.9.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       releases: releases(),
