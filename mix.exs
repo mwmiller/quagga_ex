2f22220a0dd4ff3fa07ce3b75cb3859b7d255dad
Matthew Miller <mwm@fastmail.com>
2023-08-03 09:56:41 +0200

    Update deps for better persistence

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Quagga.MixProject do
   def project do
     [
       app: :quagga,
-      version: "0.9.1",
+      version: "0.10.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       releases: releases(),
@@ -32,7 +32,7 @@ defmodule Quagga.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:baby, "~> 0.6"},
+      {:baby, "~> 0.10"},
       {:quagga_def, ">= 0.0.0"}
     ]
   end
