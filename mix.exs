21421431930fb7b569db9c296c83e61b0a01b382
Matthew Miller <mwm@fastmail.com>
2023-11-19 08:02:40 -0500

    Update to latest baby

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Quagga.MixProject do
   def project do
     [
       app: :quagga,
-      version: "0.32.0",
+      version: "0.33.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       releases: releases(),
@@ -32,7 +32,7 @@ defmodule Quagga.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:baby, "~> 0.31"},
+      {:baby, "~> 0.32"},
       {:quagga_def, ">= 0.0.0"}
     ]
   end
